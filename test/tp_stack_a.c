ac500c37624a41182161a6f281607d4fe4272a32
suzukiplan <suzukiplan.ys@gmail.com>
2016-05-22 11:07:48 +0900

    added test case of pop

--- test/tp_stack_a.c
+++ test/tp_stack_a.c
@@ -29,12 +29,72 @@ int test_push(struct vgscpu_context* c) {
     return 0;
 }
 
+int test_pop1(struct vgscpu_context* c) {
+    const char* TAG = "text_pop1";
+    unsigned short s;
+    unsigned int i;
+    unsigned char op[] = {
+        VGSCPU_OP_PUSH_A1,
+        VGSCPU_OP_POP_A1,
+        VGSCPU_OP_BRK
+    };
+
+    c->r.a = 0x12345678;
+    vgscpu_load_program(c, op, sizeof(op));
+    vgscpu_run(c);
+
+    if (c->r.a != 0x78) FAILED(TAG, __LINE__);
+    if (c->r.s != 0) FAILED(TAG, __LINE__);
+    return 0;
+}
+
+int test_pop2(struct vgscpu_context* c) {
+    const char* TAG = "text_pop2";
+    unsigned short s;
+    unsigned int i;
+    unsigned char op[] = {
+        VGSCPU_OP_PUSH_A2,
+        VGSCPU_OP_POP_A2,
+        VGSCPU_OP_BRK
+    };
+
+    c->r.a = 0x12345678;
+    vgscpu_load_program(c, op, sizeof(op));
+    vgscpu_run(c);
+
+    if (c->r.a != 0x5678) FAILED(TAG, __LINE__);
+    if (c->r.s != 0) FAILED(TAG, __LINE__);
+    return 0;
+}
+
+int test_pop4(struct vgscpu_context* c) {
+    const char* TAG = "text_pop4";
+    unsigned short s;
+    unsigned int i;
+    unsigned char op[] = {
+        VGSCPU_OP_PUSH_A4,
+        VGSCPU_OP_POP_A4,
+        VGSCPU_OP_BRK
+    };
+
+    c->r.a = 0x12345678;
+    vgscpu_load_program(c, op, sizeof(op));
+    vgscpu_run(c);
+
+    if (c->r.a != 0x12345678) FAILED(TAG, __LINE__);
+    if (c->r.s != 0) FAILED(TAG, __LINE__);
+    return 0;
+}
+
 int main() {
     struct vgscpu_context* c = (struct vgscpu_context*)vgscpu_create_context();
     if (!c) return -1;
     int result = -1;
 
     if (test_push(c)) goto END_TEST;
+    if (test_pop1(c)) goto END_TEST;
+    if (test_pop2(c)) goto END_TEST;
+    if (test_pop4(c)) goto END_TEST;
 
     result = 0;
     puts("success");
