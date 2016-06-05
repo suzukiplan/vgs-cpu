2c60673f7a948cefe1a1de8de666ece2a7340b99
suzukiplan <suzukiplan.ys@gmail.com>
2016-06-05 20:05:24 +0900

    プログラムメモリの検査を実装: INC

--- test/tp_inc.c
+++ test/tp_inc.c
@@ -1,11 +1,46 @@
 #include "tp.h"
 
+int test_program_memory(struct vgscpu_context *c)
+{
+    unsigned char op1[] = {VGSCPU_OP_INC_A, VGSCPU_OP_BRK};
+    unsigned char op2[] = {VGSCPU_OP_INC_B, VGSCPU_OP_BRK};
+    unsigned char op3[] = {VGSCPU_OP_INC_C, VGSCPU_OP_BRK};
+    unsigned char op4[] = {VGSCPU_OP_INC_D, VGSCPU_OP_BRK};
+
+    vgscpu_load_program(c, op1, sizeof(op1));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op1, sizeof(op1) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000001"), 0);
+
+    vgscpu_load_program(c, op2, sizeof(op2));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op2, sizeof(op2) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000001"), 0);
+
+    vgscpu_load_program(c, op3, sizeof(op3));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op3, sizeof(op3) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000001"), 0);
+
+    vgscpu_load_program(c, op4, sizeof(op4));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op4, sizeof(op4) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000001"), 0);
+
+    return 0;
+}
+
 int test_inc_a(struct vgscpu_context *c)
 {
     unsigned char op[] = {VGSCPU_OP_INC_A, VGSCPU_OP_BRK};
 
     vgscpu_load_program(c, op, sizeof(op));
 
+    c->r.a = 0;
     TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
     TEST(__FILE__, __LINE__, c->r.a, 1);
     TEST(__FILE__, __LINE__, c->f.z, 0);
@@ -35,6 +70,7 @@ int test_inc_b(struct vgscpu_context *c)
 
     vgscpu_load_program(c, op, sizeof(op));
 
+    c->r.b = 0;
     TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
     TEST(__FILE__, __LINE__, c->r.b, 1);
     TEST(__FILE__, __LINE__, c->f.z, 0);
@@ -64,6 +100,7 @@ int test_inc_c(struct vgscpu_context *c)
 
     vgscpu_load_program(c, op, sizeof(op));
 
+    c->r.c = 0;
     TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
     TEST(__FILE__, __LINE__, c->r.c, 1);
     TEST(__FILE__, __LINE__, c->f.z, 0);
@@ -93,6 +130,7 @@ int test_inc_d(struct vgscpu_context *c)
 
     vgscpu_load_program(c, op, sizeof(op));
 
+    c->r.d = 0;
     TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
     TEST(__FILE__, __LINE__, c->r.d, 1);
     TEST(__FILE__, __LINE__, c->f.z, 0);
@@ -122,6 +160,7 @@ int main()
     if (!c) return -1;
     int result = -1;
 
+    if (test_program_memory(c)) goto END_TEST;
     if (test_inc_a(c)) goto END_TEST;
     if (test_inc_b(c)) goto END_TEST;
     if (test_inc_c(c)) goto END_TEST;
