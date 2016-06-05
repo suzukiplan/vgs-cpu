b1c7c84f7d97ffccb45ae7515c06efd256f8a830
suzukiplan <suzukiplan.ys@gmail.com>
2016-06-05 15:48:51 +0900

    added test case: jnz

--- test/tp_jz_jnz.c
+++ test/tp_jz_jnz.c
@@ -18,13 +18,11 @@ int test_jz(struct vgscpu_context *c)
     vgscpu_run(c);
     if (c->r.a != 0x00) FAILED(TAG, __LINE__);
     if (c->r.b != 0x00) FAILED(TAG, __LINE__);
-    if (c->f.z != 0x01) FAILED(TAG, __LINE__);
 
     vgscpu_load_program(c, op2, sizeof(op2));
     vgscpu_run(c);
     if (c->r.a != 0x01) FAILED(TAG, __LINE__);
     if (c->r.b != 0x01) FAILED(TAG, __LINE__);
-    if (c->f.z != 0x00) FAILED(TAG, __LINE__);
     return 0;
 }
 
@@ -38,13 +36,11 @@ int test_jz_1(struct vgscpu_context *c)
     vgscpu_run(c);
     if (c->r.a != 0x00) FAILED(TAG, __LINE__);
     if (c->r.b != 0x00) FAILED(TAG, __LINE__);
-    if (c->f.z != 0x01) FAILED(TAG, __LINE__);
 
     vgscpu_load_program(c, op2, sizeof(op2));
     vgscpu_run(c);
     if (c->r.a != 0x01) FAILED(TAG, __LINE__);
     if (c->r.b != 0x01) FAILED(TAG, __LINE__);
-    if (c->f.z != 0x00) FAILED(TAG, __LINE__);
     return 0;
 }
 
@@ -58,13 +54,65 @@ int test_jz_2(struct vgscpu_context *c)
     vgscpu_run(c);
     if (c->r.a != 0x00) FAILED(TAG, __LINE__);
     if (c->r.b != 0x00) FAILED(TAG, __LINE__);
-    if (c->f.z != 0x01) FAILED(TAG, __LINE__);
 
     vgscpu_load_program(c, op2, sizeof(op2));
     vgscpu_run(c);
     if (c->r.a != 0x01) FAILED(TAG, __LINE__);
     if (c->r.b != 0x01) FAILED(TAG, __LINE__);
-    if (c->f.z != 0x00) FAILED(TAG, __LINE__);
+    return 0;
+}
+
+int test_jnz(struct vgscpu_context *c)
+{
+    const char *TAG = "test_jnz";
+    unsigned char op1[] = {VGSCPU_OP_LD_A_1, 0x01, VGSCPU_OP_LD_B_1, 0x00, VGSCPU_OP_DEC_A, VGSCPU_OP_JNZ, 0x0b, 0x00, 0x00, 0x00, VGSCPU_OP_INC_B, VGSCPU_OP_BRK};
+    unsigned char op2[] = {VGSCPU_OP_LD_A_1, 0x02, VGSCPU_OP_LD_B_1, 0x00, VGSCPU_OP_DEC_A, VGSCPU_OP_JNZ, 0x0b, 0x00, 0x00, 0x00, VGSCPU_OP_INC_B, VGSCPU_OP_BRK};
+
+    vgscpu_load_program(c, op1, sizeof(op1));
+    vgscpu_run(c);
+    if (c->r.a != 0x00) FAILED(TAG, __LINE__);
+    if (c->r.b != 0x01) FAILED(TAG, __LINE__);
+
+    vgscpu_load_program(c, op2, sizeof(op2));
+    vgscpu_run(c);
+    if (c->r.a != 0x01) FAILED(TAG, __LINE__);
+    if (c->r.b != 0x00) FAILED(TAG, __LINE__);
+    return 0;
+}
+
+int test_jnz_1(struct vgscpu_context *c)
+{
+    const char *TAG = "test_jnz_1";
+    unsigned char op1[] = {VGSCPU_OP_LD_A_1, 0x01, VGSCPU_OP_LD_B_1, 0x00, VGSCPU_OP_DEC_A, VGSCPU_OP_JNZ_1, 0x01, VGSCPU_OP_INC_B, VGSCPU_OP_BRK};
+    unsigned char op2[] = {VGSCPU_OP_LD_A_1, 0x02, VGSCPU_OP_LD_B_1, 0x00, VGSCPU_OP_DEC_A, VGSCPU_OP_JNZ_1, 0x01, VGSCPU_OP_INC_B, VGSCPU_OP_BRK};
+
+    vgscpu_load_program(c, op1, sizeof(op1));
+    vgscpu_run(c);
+    if (c->r.a != 0x00) FAILED(TAG, __LINE__);
+    if (c->r.b != 0x01) FAILED(TAG, __LINE__);
+
+    vgscpu_load_program(c, op2, sizeof(op2));
+    vgscpu_run(c);
+    if (c->r.a != 0x01) FAILED(TAG, __LINE__);
+    if (c->r.b != 0x00) FAILED(TAG, __LINE__);
+    return 0;
+}
+
+int test_jnz_2(struct vgscpu_context *c)
+{
+    const char *TAG = "test_jnz_2";
+    unsigned char op1[] = {VGSCPU_OP_LD_A_1, 0x01, VGSCPU_OP_LD_B_1, 0x00, VGSCPU_OP_DEC_A, VGSCPU_OP_JNZ_2, 0x01, 0x00, VGSCPU_OP_INC_B, VGSCPU_OP_BRK};
+    unsigned char op2[] = {VGSCPU_OP_LD_A_1, 0x02, VGSCPU_OP_LD_B_1, 0x00, VGSCPU_OP_DEC_A, VGSCPU_OP_JNZ_2, 0x01, 0x00, VGSCPU_OP_INC_B, VGSCPU_OP_BRK};
+
+    vgscpu_load_program(c, op1, sizeof(op1));
+    vgscpu_run(c);
+    if (c->r.a != 0x00) FAILED(TAG, __LINE__);
+    if (c->r.b != 0x01) FAILED(TAG, __LINE__);
+
+    vgscpu_load_program(c, op2, sizeof(op2));
+    vgscpu_run(c);
+    if (c->r.a != 0x01) FAILED(TAG, __LINE__);
+    if (c->r.b != 0x00) FAILED(TAG, __LINE__);
     return 0;
 }
 
@@ -77,6 +125,9 @@ int main()
     if (test_jz(c)) goto END_TEST;
     if (test_jz_1(c)) goto END_TEST;
     if (test_jz_2(c)) goto END_TEST;
+    if (test_jnz(c)) goto END_TEST;
+    if (test_jnz_1(c)) goto END_TEST;
+    if (test_jnz_2(c)) goto END_TEST;
 
     result = 0;
     puts("success");
