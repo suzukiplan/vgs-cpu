b093dc9ed225686468ba3e9c0beac506a07e4554
suzukiplan <suzukiplan.ys@gmail.com>
2016-06-09 23:46:04 +0900

    プログラムメモリの検査を実装: LDC, STC, LDD, STD

--- test/tp_ldst_c.c
+++ test/tp_ldst_c.c
@@ -1,5 +1,73 @@
 #include "tp.h"
 
+int test_program_memory(struct vgscpu_context *c)
+{
+    unsigned char op1[] = {VGSCPU_OP_LD_C_1, 0x00, VGSCPU_OP_BRK};
+    unsigned char op2[] = {VGSCPU_OP_LD_C_2, 0x00, 0x00, VGSCPU_OP_BRK};
+    unsigned char op3[] = {VGSCPU_OP_LD_C_4, 0x00, 0x00, 0x00, 0x00, VGSCPU_OP_BRK};
+    unsigned char op4[] = {VGSCPU_OP_LD_C_A, VGSCPU_OP_BRK};
+    unsigned char op5[] = {VGSCPU_OP_LD_C_B, VGSCPU_OP_BRK};
+    unsigned char op6[] = {VGSCPU_OP_LD_C_D, VGSCPU_OP_BRK};
+    unsigned char op7[] = {VGSCPU_OP_ST_C_M1, 0x08, 0x00, 0x00, 0x00, VGSCPU_OP_BRK};
+    unsigned char op8[] = {VGSCPU_OP_ST_C_M2, 0x08, 0x00, 0x00, 0x00, VGSCPU_OP_BRK};
+    unsigned char op9[] = {VGSCPU_OP_ST_C_M4, 0x08, 0x00, 0x00, 0x00, VGSCPU_OP_BRK};
+
+    vgscpu_load_program(c, op1, sizeof(op1));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op1, sizeof(op1) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000002"), 0);
+
+    vgscpu_load_program(c, op2, sizeof(op2));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op2, sizeof(op2) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000003"), 0);
+
+    vgscpu_load_program(c, op3, sizeof(op3));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op3, sizeof(op3) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000005"), 0);
+
+    vgscpu_load_program(c, op4, sizeof(op4));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op4, sizeof(op4) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000001"), 0);
+
+    vgscpu_load_program(c, op5, sizeof(op5));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op5, sizeof(op5) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000001"), 0);
+
+    vgscpu_load_program(c, op6, sizeof(op6));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op6, sizeof(op6) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000001"), 0);
+
+    vgscpu_load_program(c, op7, sizeof(op7));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op7, sizeof(op7) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000005"), 0);
+
+    vgscpu_load_program(c, op8, sizeof(op8));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op8, sizeof(op8) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000005"), 0);
+
+    vgscpu_load_program(c, op9, sizeof(op9));
+    TEST(__FILE__, __LINE__, vgscpu_run(c), 0);
+    vgscpu_load_program(c, op9, sizeof(op9) - 1);
+    TEST(__FILE__, __LINE__, vgscpu_run(c), -1);
+    TEST(__FILE__, __LINE__, strcmp(c->error, "OUT OF PROGRAM MEMORY: $00000005"), 0);
+    return 0;
+}
+
 int test_ld_c_literal(struct vgscpu_context *c)
 {
     unsigned short s;
@@ -117,6 +185,7 @@ int main()
     if (!c) return -1;
     int result = -1;
 
+    if (test_program_memory(c)) goto END_TEST;
     if (test_ld_c_literal(c)) goto END_TEST;
     if (test_ld_c_registry(c)) goto END_TEST;
     if (test_ld_c_memory(c)) goto END_TEST;
