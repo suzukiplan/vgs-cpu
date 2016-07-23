86d0584841df12c4eb4561520609f18a3535a215
suzukiplan <suzukiplan.ys@gmail.com>
2016-07-23 13:15:29 +0900

    added test: LD D from register address (ASM)

--- src/test/asm_ld_st_d.asm
+++ src/test/asm_ld_st_d.asm
@@ -68,6 +68,58 @@ start:
     CMP     D, $C001
     JNE     test-failed
 
+    // レジスタ指定アドレスからのload
+    LD      A, $12345678
+    ST      A, [0]
+    LD      A, $90abcdef
+    ST      A, [4]
+
+    LD      A, 4
+    LD      D, [A]O
+    CMP     D, $ef
+    JNE     test-failed
+    LD      D, [A]H
+    CMP     D, $cdef
+    JNE     test-failed
+    LD      D, [A]
+    CMP     D, $90abcdef
+    JNE     test-failed
+
+    LD      B, 4
+    LD      D, [B]O
+    CMP     D, $ef
+    JNE     test-failed
+    LD      D, [B]H
+    CMP     D, $cdef
+    JNE     test-failed
+    LD      D, [B]
+    CMP     D, $90abcdef
+    JNE     test-failed
+
+    LD      C, 4
+    LD      D, [C]O
+    CMP     D, $ef
+    JNE     test-failed
+    LD      D, [C]H
+    CMP     D, $cdef
+    JNE     test-failed
+    LD      D, [C]
+    CMP     D, $90abcdef
+    JNE     test-failed
+
+    LD      D, 4
+    LD      D, [D]O
+    CMP     D, $ef
+    JNE     test-failed
+    LD      D, 4
+    LD      D, [D]H
+    CMP     D, $cdef
+    JNE     test-failed
+    LD      D, 4
+    LD      D, [D]
+    CMP     D, $90abcdef
+    JNE     test-failed
+
     LD      D, 1
     BRK
 
